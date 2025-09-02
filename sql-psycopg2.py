90cda8d40f6add2c4042b8212809fe9643f82040
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-09-02 12:57:34 +0100

    Refactor sql-psycopg2.py to improve code organization and readability

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -1,5 +1,26 @@
 import psycopg2
 
 
+
+
+# Connect to the database
 connection = psycopg2.connect(database="chinook")
+
+
+# Create a cursor object of the database
 cursor = connection.cursor()
+
+# Fetch all results
+results = cursor.fetchall()
+
+# fetch the single result
+# results = cursor.fetchone()
+
+# close the connection
+
+connection.close()
+
+# print the result
+
+for result in results:
+    print(result)
